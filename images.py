9c8632745096e0a99a442d7990972c219a0a5623
rigitty <yararsamet04@gmail.com>
2026-09-11 10:53:38 +0300

    refactor: reorganize project assets

--- images.py
+++ images.py
@@ -5,7 +5,9 @@ def print_bits(value: int) -> str:
     return format(value, "08b")
 
 
-def create_sample_image(path: str = "sample.png", size: int = 100) -> None:
+def create_sample_image(path: str = "samples/sample.png", size: int = 100) -> None:
+    import os
+    os.makedirs(os.path.dirname(path), exist_ok=True)
     image = Image.new("RGB", (size, size), color=(0, 128, 255))
     for y in range(size):
         for x in range(size):
@@ -16,7 +18,7 @@ def create_sample_image(path: str = "sample.png", size: int = 100) -> None:
 
 def main() -> None:
     create_sample_image()
-    image = Image.open("sample.png")
+    image = Image.open("samples/sample.png")
 
     x, y = 50, 50
     r, g, b = image.getpixel((x, y))
